b996ea63f2f369274a1f254a81747f3cb6d35f8e
Psy Coder <psycoder@lenovo>
2020-06-12 01:15:05 +0600

    taking the sqlcamp landing page live with 4 sections

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+function toggle() {
+    const header = this.document.querySelector('header');
+    header.classList.toggle('active');
+
+}
+
+// Admission FAQ section
+const accordianItemHeaders = document.querySelectorAll('.accordian-item-header');
+
+accordianItemHeaders.forEach(accordianItemHeader => {
+    accordianItemHeader.addEventListener('click', event => {
+        const currentlyActiveAccordianItemHeader = document.querySelector('.accordian-item-header.active');
+
+        if( currentlyActiveAccordianItemHeader && currentlyActiveAccordianItemHeader !== accordianItemHeader ) {
+          currentlyActiveAccordianItemHeader.classList.toggle('active');
+          currentlyActiveAccordianItemHeader.nextElementSibling.style.maxHeight = 0; 
+        }
+
+        accordianItemHeader.classList.toggle('active');
+        const accordianItemBody = accordianItemHeader.nextElementSibling;
+        if(accordianItemHeader.classList.contains('active')){
+            accordianItemBody.style.maxHeight = accordianItemBody.scrollHeight + "px"
+        } else {
+            accordianItemBody.style.maxHeight = 0;
+        }
+    })
+});
